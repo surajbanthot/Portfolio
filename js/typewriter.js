c72a450530085b1435476354e16f89312c19f50e
Suraj_Banthot <surajbanthot@gmail.com>
2020-06-03 13:21:15 +0530

    typewriter effect with change in delay speed

--- js/typewriter.js
+++ js/typewriter.js
@@ -28,7 +28,7 @@ class TypeWriter {
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     // Initial Type Speed
-    let typeSpeed = 300;
+    let typeSpeed = 150;
 
     if (this.isDeleting) {
       typeSpeed /= 2;
@@ -45,7 +45,7 @@ class TypeWriter {
       // Move to next word
       this.wordIndex++;
       // Pause before start typing
-      typeSpeed = 500;
+      typeSpeed = 300;
     }
 
     setTimeout(() => this.type(), typeSpeed);
